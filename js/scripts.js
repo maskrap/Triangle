8dc06e2d018e457a2950729ac3abc81151a1600d
Rachael Bermudes and Sam Park <rachael@>
2016-06-02 14:54:32 -0700

    It will added HTML and JS components

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("form#questions").submit(function(event) {
+    event.preventDefault();
+
+    var side1 = parseInt($("#side1").val());
+    var side2 = parseInt($("#side2").val());
+    var side3 = parseInt($("#side3").val());
+
+    if ("side1" === "side2" === "side3") {
+      $('#equ').show();
+
+    } else if ("side1" === "side2" || "side1" === "side3" || "side2" === "side3") {
+        $('#iso').show();
+        }
+
+      else if ("side1" !=== "side2" && "side2" !=== "side3" && "side1" !=== "side3") {
+        $('#sca').show();
+      }
+
+      else {
+        $('#not').show();
+      }
+
+
+  });
+});
